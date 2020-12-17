577274143413424705bd243694a42748afc49a62
Juan Vivas <juanvivas@alphrtechnology.co.uk>
2020-12-17 10:29:18 +0000

    The light wiring finished- time to code!

--- TheLight/src/main.cpp
+++ TheLight/src/main.cpp
@@ -1,7 +1,7 @@
 #include <FastLED.h>
 
 // How many leds in your strip?
-#define NUM_LEDS 64
+#define NUM_LEDS 116
 
 // For led chips like WS2812, which have a data line, ground, and power, you just
 // need to define DATA_PIN.  For led chipsets that are SPI based (four wires - data, clock,
@@ -10,33 +10,53 @@
 #define DATA_PIN 3
 #define CLOCK_PIN 13
 #define LED 2
-#define ButtonLamp1 19
-#define ButtonSW1 4
-#define pintest 23
+#define JoystickLamp 27
 
-  // Potentiometer is connected to GPIO 34 (Analog ADC1_CH6) 
+
+// Potentiometer is connected to GPIO 34 (Analog ADC1_CH6) 
 const int potPin = 34;
 
 // Define the array of leds
 CRGB leds[NUM_LEDS];
+int ButtonSwitchesID [5]={19,23,35,36,39};
+int ButtonLampsID [5]={4,5,16,17,18};
+int JoystickID[4]={25,26,32,33};
 
-void setup() {     
+
+void setup() {  
+
   FastLED.addLeds<WS2812B, DATA_PIN, RGB>(leds, NUM_LEDS);  // GRB ordering is typical    
   //Stayalive Signal
   pinMode(LED,OUTPUT);
 
-  //Joystick lights
-  pinMode(pintest,OUTPUT);
+ 
 
-//Buttons
-  pinMode(ButtonLamp1,OUTPUT);
-  pinMode(ButtonSW1,INPUT_PULLUP);
+  pinMode(ButtonLampsID[0],OUTPUT);
+  pinMode(ButtonLampsID[1],OUTPUT);
+  pinMode(ButtonLampsID[2],OUTPUT);
+  pinMode(ButtonLampsID[3],OUTPUT);
+  pinMode(ButtonLampsID[4],OUTPUT);
 
+   //Joystick lights
+  pinMode(JoystickLamp,OUTPUT);
 
-    // Turn the LED on, then pause
+ //Buttons
+  pinMode(ButtonSwitchesID[0],INPUT_PULLUP);
+  pinMode(ButtonSwitchesID[1],INPUT_PULLUP);
+  pinMode(ButtonSwitchesID[2],INPUT_PULLUP);
+  pinMode(ButtonSwitchesID[3],INPUT_PULLUP);
+  pinMode(ButtonSwitchesID[4],INPUT_PULLUP);
+
+   pinMode(JoystickID[0],INPUT_PULLUP);
+   pinMode(JoystickID[1],INPUT_PULLUP);
+   pinMode(JoystickID[2],INPUT_PULLUP);
+   pinMode(JoystickID[3],INPUT_PULLUP);
+
+  //Turn the LED on, then pause
   leds[0] = CRGB::Red;
   FastLED.show();
   delay(10);
+
   // Now turn the LED off, then pause
   leds[0] = CRGB::Black;
   FastLED.show();
@@ -49,65 +69,114 @@ void setup() {
 
 void fadeall() { for(int i = 0; i < NUM_LEDS; i++) { leds[i].nscale8(250); } }
 
+int i;
+int potValue;
+int nBrightness=255;
+
 void loop() { 
+
+  delay(600);
   
- static uint8_t hue = 0;
+    static uint8_t hue = 0;
+    //digitalWrite(ButtonLampsID[0], HIGH);
+    //digitalWrite(ButtonLampsID[1], HIGH);
+    //digitalWrite(ButtonLampsID[2], HIGH);
+    //digitalWrite(ButtonLampsID[3], HIGH);
+    //digitalWrite(ButtonLampsID[4], HIGH);
+    digitalWrite(JoystickLamp, HIGH);
 
-// variable for storing the potentiometer value
-//int potValue = 0;
-
-    if(digitalRead(ButtonSW1)==LOW) {
-      digitalWrite(ButtonLamp1, HIGH);
-      }
-     else
-      {
-        digitalWrite(ButtonLamp1, LOW);
-      }
-      
     
-
+       
   //Infinite loop
   do {  
+
+
+           digitalWrite(ButtonLampsID[0], HIGH);
+           digitalWrite(ButtonLampsID[1], HIGH);
+           digitalWrite(ButtonLampsID[2], HIGH);
+           digitalWrite(ButtonLampsID[3], HIGH);
+           digitalWrite(ButtonLampsID[4], HIGH);
+           digitalWrite(JoystickLamp, HIGH);
+
+
+   
+//i=0;
+  //for(i=0;i<=3;i++){
+
+    if(digitalRead(JoystickID[0])==LOW) {
+      digitalWrite(ButtonLampsID[0], HIGH);
+      //Serial.println("Low");
+    }
+    else{
+      digitalWrite(ButtonLampsID[0], LOW);
+      //Serial.println("High");
+    }
+ // }
+      
+
+      
+      
+
     for(int i = 0; i < NUM_LEDS; i++) {
       // Set the i'th led to red 
-      leds[i] = CHSV(hue++, 255, 50);
+      leds[i] = CHSV(hue++, 255, nBrightness);
       // Show the leds
       FastLED.show(); 
       // now that we've shown the leds, reset the i'th led to black
       // leds[i] = CRGB::Black;
       fadeall();
       // Wait a little bit before we loop around and do it again
-      delay(100);
+      delay(5);
     }
+
+         
+           digitalWrite(ButtonLampsID[0], LOW);
+           digitalWrite(ButtonLampsID[1], LOW);
+           digitalWrite(ButtonLampsID[2], LOW);
+           digitalWrite(ButtonLampsID[3], LOW);
+           digitalWrite(ButtonLampsID[4], LOW); 
+            digitalWrite(JoystickLamp, LOW); 
 
     // Now go in the other direction.  
 	  for(int i = (NUM_LEDS)-1; i >= 0; i--) {
       // Set the i'th led to red 
-      leds[i] = CHSV(hue++, 255, 50);
+      leds[i] = CHSV(hue++, 255, nBrightness);
       // Show the leds
       FastLED.show();
       // now that we've shown the leds, reset the i'th led to black
       // leds[i] = CRGB::Black;
       fadeall();
       // Wait a little bit before we loop around and do it again
-      delay(10);
-	}
+      delay(5);
+	  }
 
-    //Joystick lights
-    digitalWrite(LED,HIGH);
-    digitalWrite(pintest,HIGH);
-    delay(1500);
-    digitalWrite(LED,LOW);
-    digitalWrite(pintest,LOW);
-    delay(1500);
-
-    //Read inputs
-
-    //Read ADC
     potValue = analogRead(potPin);
+    nBrightness=potValue/16;
     Serial.println(potValue);
-    delay(500);
+    delay(50);
 
+
+    for(int i=0;i<5;i++){
+    if(digitalRead(ButtonSwitchesID[i])==LOW) {
+            //digitalWrite(ButtonLampsID[i], HIGH);
+            Serial.print("Button ");
+            Serial.println(i);
+    }
+    else{
+      //digitalWrite(ButtonLampsID[i], LOW);
+    }
+  }
+
+   for(int i=0;i<4;i++){
+    if(digitalRead(JoystickID[i])==LOW) {
+            //digitalWrite(ButtonLampsID[i], HIGH);
+            Serial.print("Joy ");
+            Serial.println(i);
+    }
+    else{
+      //digitalWrite(ButtonLampsID[i], LOW);
+    }
+  }
 
   }
   while (true);
